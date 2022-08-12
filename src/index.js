9205d9cc5652cad51772937b605505e46c95fd22
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-13 00:26:04 +0300

    code version 2

--- src/index.js
+++ src/index.js
@@ -37,32 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(encodedText) {
-    let encodedChar = '';
-    let morseChar = '';
-    let decodedChar = '';
-    let decodedText = '';
-    let remainingText = encodedText;
+// function decode(expr) {
+//     let encodedChar = '';
+//     let morseChar = '';
+//     let decodedChar = '';
+//     let decodedText = '';
+//     let remainingText = expr;
 
-    while (remainingText) {
-        encodedChar = remainingText.substring(0, 10);
+//     while (remainingText) {
+//         encodedChar = remainingText.substring(0, 10);
         
-        if (encodedChar == '**********') {
-            decodedChar = ' ';
-        } else {
-            morseChar = encodedChar
-                .replace(/00/g, '')
-                .replace(/10/g, '.')
-                .replace(/11/g, '-');
+//         if (expr == '**********') {
+//             decodedChar = ' ';
+//         } else {
+//             morseChar = encodedChar
+//                 .replace(/00/g, '')
+//                 .replace(/10/g, '.')
+//                 .replace(/11/g, '-');
     
-            decodedChar = MORSE_TABLE[morseChar];  
-        }
+//             decodedChar = MORSE_TABLE[morseChar];  
+//         }
 
-        decodedText += decodedChar;
-        remainingText = remainingText.substring(10);
-    } 
+//         decodedText += decodedChar;
+//         remainingText = remainingText.substring(10);
+//     } 
     
-    return decodedText;
+//     return decodedText;
+// }
+
+function decode (morseCode) {
+    return morseCode.split(' ').map(a => a.split(' ').map(b => MORSE_TABLE[b]).join(''); 
 }
 module.exports = {
     decode
