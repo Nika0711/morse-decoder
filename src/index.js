9a70c3a53619d3e6ad03b07f169bb16aa1381490
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-13 00:10:19 +0300

    new code

--- src/index.js
+++ src/index.js
@@ -37,10 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode(encodedText) {
+    let encodedChar = '';
+    let morseChar = '';
+    let decodedChar = '';
+    let decodedText = '';
+    let remainingText = encodedText;
 
+    while (remainingText) {
+        encodedChar = remainingText.substring(0, 10);
+        
+        if (encodedChar == '**********') {
+            decodedChar = ' ';
+        } else {
+            morseChar = encodedChar
+                .replace(/00/g, '')
+                .replace(/10/g, '.')
+                .replace(/11/g, '-');
+    
+            decodedChar = MORSE_TABLE[morseChar];  
+        }
+
+        decodedText += decodedChar;
+        remainingText = remainingText.substring(10);
+    } 
+    
+    return decodedText;
+}
 module.exports = {
     decode
 }
